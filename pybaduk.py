b9b6f3b9b227e99ae4f8f18d182d1caa930d0cac
Lukas Lindroos <themorgawr@hotmail.com>
2012-11-16 08:16:06 +0100

    Fixes to player class and example sorting of players.

--- pybaduk.py
+++ pybaduk.py
@@ -42,10 +42,9 @@ class Player(object):
                     given_name, family_name).encode('ascii', errors='egd')
             self.rel_player_path = os.path.join(PlayerList.path, filename)
             self.fq_player_path = os.path.join(repo.path, self.rel_player_path) 
-            self.fq_playerdir_path = os.path.join(repo.path, PlayerList.path) 
             json.dump(params, open(self.fq_player_path, 'w'))
             repo.stage(self.rel_player_path)
-            repo.do_commit('Adding new player.', committer = 'Should have some session id here.')
+            repo.do_commit('appending new player.', committer = 'Should have some session id here.')
 
 
     def __str__(self):
@@ -66,16 +65,44 @@ class Player(object):
 
     def get_given_name(self):
         return self.get_player_property('given_name')
+    def get_family_name(self):
+        return self.get_player_property('family_name')
+    def get_rank(self):
+        return self.get_player_property('rank')
+    def set_rank(self, rank):
+        self.set_player_property('rank', rank)
 
     def set_given_name(self, given_name):
-        data = json.load(open(self.fq_player_path))
-        data['given_name'] = given_name
-        print json.dumps(data)
-        json.dump(data, open(self.fq_player_path, 'w'))
+        newfilename = u'{0}_{1}'.format(
+                given_name, self.get_family_name()).encode('ascii', errors='egd')
+        
+        print 'staging',self.rel_player_path
+
+        new_rel_player_path = os.path.join(PlayerList.path, newfilename)
+        new_fq_player_path = os.path.join(repo.path, new_rel_player_path)
+        os.rename(self.fq_player_path, new_fq_player_path)
         repo.stage(self.rel_player_path)
-        repo.do_commit('Modifying player.', committer = 'Should have some session id here.')
+        self.fq_player_path = new_fq_player_path
+        self.rel_player_path = new_rel_player_path
+
+        self.set_player_property('given_name', given_name)
+
+    def set_family_name(self, family_name):
+        newfilename = u'{0}_{1}'.format(
+                self.get_given_name(), family_name).encode('ascii', errors='egd')
+        
+        new_rel_player_path = os.path.join(PlayerList.path, newfilename)
+        new_fq_player_path = os.path.join(repo.path, new_rel_player_path)
+        os.rename(self.fq_player_path, new_fq_player_path)
+        repo.stage(self.rel_player_path)
+        self.fq_player_path = new_fq_player_path
+        self.rel_player_path = new_rel_player_path
+
+        self.set_player_property('family_name', family_name)
 
     given_name = property(get_given_name, set_given_name)
+    family_name = property(get_family_name, set_family_name)
+    rank = property(get_rank, set_rank)
 
     def get_player_property(self, key):
         data = json.load(open(self.fq_player_path))
@@ -98,7 +125,6 @@ class PlayerList(object):
         self.fq_playerdir_path = os.path.join(repo.path, PlayerList.path)
         if not os.path.isdir(self.fq_playerdir_path):
             os.mkdir(self.fq_playerdir_path)
-        print self.fq_playerdir_path
 
     def __len__(self):
         return len(os.listdir(self.fq_playerdir_path))
@@ -107,7 +133,7 @@ class PlayerList(object):
         for player in os.listdir(self.fq_playerdir_path):
             yield Player(repo, path=os.path.join(self.path, player))
 
-    def add(self, params):
+    def append(self, params):
         Player(self.repo, params=params)
 
 
@@ -119,13 +145,18 @@ if __name__ == "__main__":
         repo = Repo.init("/data/lindroos/pybaduk/turn")
     p = PlayerList(repo)
 #     print len(p)
-    p.add({'given_name': u'Magnus', 'family_name': u'Sandén'})
-    p.add({'given_name': u'Robert', 'family_name': u'Åhs'})
+    p.append({'given_name': u'Robert', 'family_name': u'Åhs'})
+    p.append({'given_name': u'Eskil', 'family_name': u'Varenius'})
+    p.append({'given_name': u'Lukas', 'family_name': u'Lindroos'})
+    p.append({'given_name': u'Erik', 'family_name': u'Änterhake'})
+    p.append({'given_name': u'Magnus', 'family_name': u'Sandén'})
+    p.append({'given_name': u'Niklas', 'family_name': u'Örjansson'})
 
-    for player in p:
+#     for player in p:
+#         print unicode(player)
+
+    list(p)[0].rank = '6K'
+    for player in sorted(list(p), key=lambda player: player.family_name):
+#         player.given_name = 'Jonas'
         print unicode(player)
-
-    for player in p:
-        player.given_name = 'Jonas'
-        print player.given_name
 #         print player.get_player_property('family_name')
