150f21ca8fbe75edf5e2e9c54837545fc152dfeb
Magnus Sandén <thefreid@gmail.com>
2012-11-18 21:55:31 +0100

    Added indices to all GitEntries. Added Tournament class.
    Indices should probably be moved to GitEntry.

--- git.py
+++ git.py
@@ -7,28 +7,19 @@ class GitLoadError(Exception):
 
 
 class GitEntry(object):
-    def __init__(self, repo, params=None, git_table=None, index=None):
+    def __init__(self, repo, git_table, filename, params=None):
         super(GitEntry, self).__init__()
         self.repo = repo
-        # Checks like this aren't very pythonic:
-        # Think duck typing: Anything that supports the subset of str interface
-        # that we use here should be supported by this class. For example
-        # unicode strings
-        #if not isinstance(git_table, str):
-        #    raise GitLoadError('Invalid git_table (players, pairings, etc...)'
-        #                       'specified.')
-        #elif not isinstance(index, str):
-        #    raise GitLoadError('Invalid index (filename) specified: "{0}".'
-        #                       .format(index))
+        self.session_id = '1337'
 
-        self.rel_path = os.path.join(git_table, index)
+        self.rel_path = os.path.join(git_table, filename)
         self.fq_path = os.path.join(repo.path, self.rel_path) 
 
         if params:
             json.dump(params, open(self.fq_path, 'w'))
             repo.stage(self.rel_path)
-            repo.do_commit('Updating entry {0} in {1}.'.format(index, git_table),
-                    committer = 'Should have some session id here.')
+            repo.do_commit('Updating entry {0} in {1}.'.format(
+                filename, git_table), committer=self.session_id)
 
     def getindex(self):
         return os.path.split(self.fq_path)[-1]
@@ -39,22 +30,30 @@ class GitEntry(object):
     def remove(self):
         os.remove(self.fq_path)
         self.repo.stage(self.rel_path)
-        self.repo.do_commit('removing', committer = 'Should have some session id here.')
+        commit_msg = 'Removing {} {}'.format(type(self).__name__.lower(), self)
+        self.repo.do_commit(commit_msg, committer=self.session_id)
 
     def _rename_file(self, git_table, new_index):
         new_rel_path = os.path.join(git_table, new_index)
         new_fq_path = os.path.join(repo.path, new_rel_path)
+        commit_msg = 'Renaming file {} to {}'.format(self.rel_path, new_rel_path)
         os.rename(self.fq_path, new_fq_path)
+        #TODO: Staging the file that will be removed doesn't do anything.
+        #      We need to find the equivalent of 'git rm file'
         self.repo.stage(self.rel_path)
+        self.repo.stage(new_rel_path)
         self.fq_path = new_fq_path
         self.rel_path = new_rel_path
+        self.repo.do_commit(commit_msg, committer=self.session_id)
 
     def __delitem__(self, key):
         data = json.load(open(self.fq_path))
         del data[key]
         json.dump(data, open(self.fq_path, 'w'))
+        #TODO: Staging the file that will be removed doesn't do anything.
+        #      We need to find the equivalent of 'git rm file'
         self.repo.stage(self.rel_path)
-        self.repo.do_commit('Modifying .', committer = 'Should have some session id here.')
+        self.repo.do_commit('Modifying .', committer=self.session_id)
 
     def __getitem__(self, key):
         data = json.load(open(self.fq_path))
@@ -65,4 +64,4 @@ class GitEntry(object):
         data[key] = value
         json.dump(data, open(self.fq_path, 'w'))
         self.repo.stage(self.rel_path)
-        self.repo.do_commit('Modifying .', committer = 'Should have some session id here.')
+        self.repo.do_commit('Modifying .', committer=self.session_id)
