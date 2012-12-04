48ff14fcf8cf079f1dda0032b7bebd04a7fb4098
Lukas Lindroos <themorgawr@hotmail.com>
2012-12-04 22:20:21 +0100

    Removed test from name.

---
+++ datatest.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import codecs
+
+import egdcodec
+from tournament import Tournament
+
+
+codecs.register_error('egd', egdcodec.egd_replace)
+
+
+if __name__ == "__main__":
+#     turnpath = '/home/freidrichen/tmp/turn'
+    turnpath = '/data/lindroos/pybaduk/turn'
+    tournament = Tournament(turnpath, u'Göteborg Open 2013')
+
+    players = tournament.players
+    if not players:
+        players.append({'given_name': u'Robert', 'family_name': u'Åhs'})
+        eskil_id = players.append({'given_name': u'Eskil',
+                                   'family_name': u'Varenius', 'rank': '4K'})
+        lukas_id = players.append({'given_name': u'Lukas', 'family_name': u'Lindroos',
+                                   'rank': '6K'})
+        players.append({'given_name': u'Erik', 'family_name': u'Änterhake'})
+        magnus_id = players.append({'given_name': u'Magnus', 'family_name': u'Sandén',
+                                    'rank': '7K'})
+        players.append({'given_name': u'Niklas', 'family_name': u'Örjansson'})
+
+    round0_pairs = tournament.pairings[0]
+    if not round0_pairs:
+        round0_pairs.append(magnus_id, eskil_id)
+        round0_pairs.append(lukas_id)
+
+    #for player in players:
+    #    if player['given_name'] == 'Eskil':
+    #        player.remove()
+    #    elif player['rank'] == '7K':
+    #        player['family_name'] = 'Andersson'
+
+    for player in players:
+        if player['given_name'] == 'Magnus':
+            tournament.remove_player(player)
+            break
+
+    for player in sorted(players, key=lambda player: player['family_name']):
+        print(unicode(player))
+
+    for pairing in round0_pairs:
+        print(unicode(pairing))
--- pybaduk.py
+++ pybaduk.py
@@ -1,48 +1,102 @@
 # -*- coding: utf-8 -*-
-import codecs
+import sys
+# import PyQt4
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from qttest_ui import Ui_MainWindow
+from player_tab import Ui_PlayerTab
 
+from dulwich.repo import Repo
+import dulwich.errors
+
+import codecs
 import egdcodec
 from tournament import Tournament
+
+import locale
+from functools import total_ordering
+
+class MainUIWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+        self.ui = Ui_MainWindow()
+
+        self.ui.setupUi(self)
+
+
+class PlayerTab(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+        self.ui = Ui_PlayerTab()
+
+        self.ui.setupUi(self)
+
+
+@total_ordering
+class Name(QTableWidgetItem):
+    def __init__(self, name):
+        super(Name,self).__init__(name)
+        self.name = name
+        locale.setlocale(locale.LC_ALL, "sv_SE.UTF-8")
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name.encode('ascii', errors='egd')
+    
+    def __eq__(self, other_name):
+        return locale.strcoll(self.name, other_name.name) == 0
+
+    def __lt__(self, other_name):
+        return locale.strcoll(self.name, other_name.name) < 0
 
 
 codecs.register_error('egd', egdcodec.egd_replace)
 
+def main():
+    app = QApplication(sys.argv)
+
+    turnpath = '/data/lindroos/pybaduk/turn'
+    gbgopen = Tournament(turnpath, u'Göteborg Open 2013')
+
+    myapp = MainUIWindow()
+    playerTab = PlayerTab()
+    playerlist = gbgopen.players
+    playerTab.ui.tableWidget.setRowCount(len(playerlist))
+    playerTab.ui.tableWidget.setColumnCount(3)
+    playerTab.ui.tableWidget.verticalHeader().hide()
+    playerTab.ui.tableWidget.setHorizontalHeaderLabels(['Given name', 'Family name', 'Rank'])
+
+    myapp.ui.tabWidget.clear()
+    myapp.ui.tabWidget.addTab(playerTab, "Players")
+    for (i,player) in enumerate(sorted(playerlist, key=lambda player: player['family_name'])):
+#         QTableWidgetItem(unicode(player), myapp.ui.tableWidget)
+        family_name = Name(player['family_name'])
+        given_name = Name(player['given_name'])
+#         family_name = QTableWidgetItem(Name(player['family_name']))
+#         given_name = QTableWidgetItem(Name(player['given_name']))
+        rank = player['rank']
+        if player['rank'] == None:
+            rank = 'No rank'
+        rank = QTableWidgetItem(rank)
+        playerTab.ui.tableWidget.setItem(i, 0, given_name)
+        playerTab.ui.tableWidget.setItem(i, 1, family_name)
+        playerTab.ui.tableWidget.setItem(i, 2, rank)
+    playerTab.ui.tableWidget.resizeColumnsToContents()
+    playerTab.ui.tableWidget.horizontalHeader().setStretchLastSection(True)
+    myapp.show()
+    sys.exit(app.exec_())
+#   a = QApplication(sys.argv)
+# 
+#   w = QWidget()
+#   w.resize(320, 240)
+#   w.setWindowTitle('Hello World!')
+#   w.show()
+# 
+#   sys.exit(a.exec_())
 
 if __name__ == "__main__":
-#     turnpath = '/home/freidrichen/tmp/turn'
-    turnpath = '/data/lindroos/pybaduk/turn'
-    tournament = Tournament(turnpath, u'Göteborg Open 2013')
-
-    players = tournament.players
-    if not players:
-        players.append({'given_name': u'Robert', 'family_name': u'Åhs'})
-        eskil_id = players.append({'given_name': u'Eskil',
-                                   'family_name': u'Varenius', 'rank': '4K'})
-        lukas_id = players.append({'given_name': u'Lukas', 'family_name': u'Lindroos',
-                                   'rank': '6K'})
-        players.append({'given_name': u'Erik', 'family_name': u'Änterhake'})
-        magnus_id = players.append({'given_name': u'Magnus', 'family_name': u'Sandén',
-                                    'rank': '7K'})
-        players.append({'given_name': u'Niklas', 'family_name': u'Örjansson'})
-
-    round0_pairs = tournament.pairings[0]
-    if not round0_pairs:
-        round0_pairs.append(magnus_id, eskil_id)
-        round0_pairs.append(lukas_id)
-
-    #for player in players:
-    #    if player['given_name'] == 'Eskil':
-    #        player.remove()
-    #    elif player['rank'] == '7K':
-    #        player['family_name'] = 'Andersson'
-
-    for player in players:
-        if player['given_name'] == 'Magnus':
-            tournament.remove_player(player)
-            break
-
-    for player in sorted(players, key=lambda player: player['family_name']):
-        print(unicode(player))
-
-    for pairing in round0_pairs:
-        print(unicode(pairing))
+    main()
